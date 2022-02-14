7fd4c9815ea34da04dde3a45b12a111d17dfbef8
Franck COLOMBO <franck.colombo@cnav.fr>
2022-02-14 17:51:03 +0100

    mise en page introduction et boutons

---
+++ components/ui/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full">
+    <h1>Button</h1>
+
+    <section class="main-design mt-10 mb-10 ">
+      <h2>Design Principal</h2>
+      <ul class="ml-6">
+        <li>Guide users to achieve the desired actions.</li>
+        <li>Prevent user to make mistakes.</li>
+      </ul>
+    </section>
+    <section class="types mt-10 mb-10">
+        <h3 >Common Button Types</h3>
+        <div class="bloc-desction h-16 bg-slate-300"> UI design ici</div>
+    </section>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+ul li {
+    list-style-type: circle;
+  }
+</style>
