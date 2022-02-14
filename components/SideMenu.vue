6ef612289aae49555db89c8d0ddc70855f2f1f6e
Franck COLOMBO <franck.colombo@cnav.fr>
2022-02-14 17:58:33 +0100

    commit change not commited ...

--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -5,30 +5,28 @@
         <nav>
           <ul class="side-menu">
               <li class="">
-              <div class="sub-menu_title">
+              <div class="sub-menu_title"   >
                 <NuxtLink
                   to="/design/"
                   class="block h-full w-full outline-none"
                   >Accueil</NuxtLink
                 >
               </div>
-              <ul class="sub-menu hidden flex-col">
-                <li class="sub-menu_items ml-3"></li>
-                <li class="sub-menu_items ml-3"></li>
-              </ul>
+
             </li>
             <li class="">
-              <div class="sub-menu_title">
+              <!-- <div class="sub-menu_title" @click.prevent="activeSubmenu($event)"> -->
+              <div class="sub-menu_title" >
                 <NuxtLink
-                  to="/design/parent/button"
+                  to="/design/button"
                   class="block h-full w-full outline-none"
-                  >boutons</NuxtLink
+                  >Boutons</NuxtLink
                 >
               </div>
-              <ul class="sub-menu hidden flex-col">
+              <!-- <ul class="sub-menu hidden flex-col">
                 <li class="sub-menu_items ml-3"></li>
                 <li class="sub-menu_items ml-3"></li>
-              </ul>
+              </ul> -->
             </li>
             <li class="sub-menu_title">Box</li>
             <li class="sub-menu_title">Parapgraphes</li>
@@ -40,10 +38,26 @@
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+     activeSubmenu(event){
+       console.log(event)
+        event.target.parentElement.classList.toggle("active");
+      }
+  }
+}
+</script>
+
 
 <style scoped lang="scss">
 .sub-menu_title {
-  @apply pl-3  leading-10 bg-transparent transition duration-300  hover:bg-red-200 transform cursor-pointer;
+  @apply pl-3  leading-10 bg-transparent transition duration-200  hover:text-blue-500 transform cursor-pointer;
 
   &.active {
     @apply bg-red-200;
--- pages/design/index.vue
+++ pages/design/index.vue
@@ -1,12 +1,23 @@
 <template>
-    
+
     <div class="flex pt-10 w-full">
       <side-menu class="basis-1/6 border-r-2 min-h-screen"></side-menu>
       <div class="w-full min-h-full">
         <div class="container pl-16 pr-16">
-          <h1>Presentation Page</h1>
+          <h1 class="">Introduction</h1>
+          <section class="intro">
+              <img src="/introduction.png" alt="image inroduction" srcset="" class="max-w-4xl mb-10">
+              <div>
+                  <p class="mt-5">Ant Financial has a large number of enterprise-level products. With complex scenarios, designers and developers often need to respond fast due to frequent changes in product demands and concurrent R & D workflow. Many similar contents exist in the process. Through abstraction, we could obtain some stable and highly reusable components and pages.</p>
+                    <p class="mt-5">
+                        On the other hand, with the trend of commercialization, more and more enterprise products begin to pursue better user experiences. Under this situation, Ant User-Experience Design Team builds a design system for enterprise products based on four design values of Natural, Certain, Meaningful, and Growing. It aims to uniform the user interface specs and reduce redundancies and excessive production costs, helping product designers to focus on better user experience.
+                    </p>
+              </div>
+
+          </section>
+          
         </div>
       </div>
     </div>
-  
+
 </template>
