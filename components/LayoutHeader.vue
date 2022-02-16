4d2ef90ed0dbc81c46edb4a0381242057d3333f7
Franck COLOMBO <franck.colombo@cnav.fr>
2022-02-16 11:11:51 +0100

    modification marge header
    * padding right and left
    * remove search bar

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,23 +1,18 @@
 <template>
-     <header class="flex flex-row place-items-center shadow-sm h-16">
-        <div id="logo" class="basis-1/6  text-center flex items-center gap-3">
+     <header class="flex flex-row place-items-center shadow-sm h-16 lg:pl-36 lg:pr-36">
+        <div id="logo" class=" text-center flex flex-none basis-2/6 items-center shrink-0 gap-3">
             <img src="/logo-ipsum.svg" alt="logo-ipsum"  srcset="" class="ml-5 h-10">
-            <div class="title-site" >
+            <div class="title-site text-lg font-bold" >
                 Flut Design
             </div>
         </div>
-        <div class="search-content basis-2/6">
-        <div class="search-box ml-10">
-            <input type="text" placeholder="Search" class="border h-10 rounded-sm p-3" >
-        </div>
-            
-        </div>
        
-        <div class="basis-3/6">
-            <ul class="flex gap-x-2  w-max  ">
-                <li><router-link to="/"   class="hover:text-yellow-600" >Home</router-link></li>
-                <li><nuxt-link to="/design"  class="hover:text-yellow-600">Design</nuxt-link></li>
+        <div class="basis-2/6 relative flex  justify-center shrink-1 ">
+            <ul class="flex gap-x-4  w-max  ">
+                <li><router-link to="/"   class="hover:text-blue-500" >Home</router-link></li>
+                <li><nuxt-link to="/design"  class="hover:text-blue-500">Design</nuxt-link></li>
             </ul>
         </div>
+        <div class="basis-2/6 shrink-0"></div>
     </header>
 </template>
