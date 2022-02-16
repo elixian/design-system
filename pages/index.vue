b2e3faced0c83857d4b5f16ec5b3e40c3d4e8abf
Franck COLOMBO <franck.colombo@cnav.fr>
2022-02-16 11:13:20 +0100

    design page home

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe vero officia fuga sint facilis voluptas deleniti non earum dicta dignissimos, molestias nobis maiores ex quidem. Accusantium nisi sapiente illo autem!</p>
+  <div class="w-full  ">
+    <section id="entete" class="h-80 pt-10 bg-gradient-to-b from-slate-200">
+      <h1 class="text-7xl text-center">
+        Flut Design
+      </h1>
+    </section>
+    <section id="content" class="w-full pl-60 pr-60">
+      <div>
+        <h2 class="font-light">Recommanded</h2>
+        <div class="flex gap-4 m-auto w-full">
+          <div class="h-80 w-2/3 bg-orange-200"></div>
+          <div class="flex flex-col gap-4 w-1/3">
+            <div class="h-2/4 bg-orange-200"></div>
+            <div class="h-2/4 bg-orange-200"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
 
 <script>
 export default {
