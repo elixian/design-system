33513cf070df04dd8b3e49025595a2e9309dff6b
Franck COLOMBO <franck.colombo@cnav.fr>
2022-02-23 11:28:52 +0100

    modification sidemenu

--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -1,41 +1,36 @@
 <template>
-
-    <aside class="bg-gradient-to-t from-slate-200">
-      <div class="content-nav">
-        <nav>
-          <ul class="side-menu">
-              <li class="">
-              <div class="sub-menu_title"   >
-                <NuxtLink
-                  to="/design/"
-                  class="block h-full w-full outline-none"
-                  >Accueil</NuxtLink
-                >
-              </div>
-
-            </li>
-            <li class="">
-              <!-- <div class="sub-menu_title" @click.prevent="activeSubmenu($event)"> -->
-              <div class="sub-menu_title" >
-                <NuxtLink
-                  to="/design/button"
-                  class="block h-full w-full outline-none"
-                  >Boutons</NuxtLink
-                >
-              </div>
-              <!-- <ul class="sub-menu hidden flex-col">
+  <aside class="bg-gradient-to-t from-slate-200">
+    <div class="content-nav">
+      <nav>
+        <ul class="side-menu">
+          <li class="">
+            <div class="sub-menu_title">
+              <NuxtLink to="/design/" class="block h-full w-full outline-none"
+                >Accueil</NuxtLink
+              >
+            </div>
+          </li>
+          <li class="">
+            <!-- <div class="sub-menu_title" @click.prevent="activeSubmenu($event)"> -->
+            <div class="sub-menu_title">
+              <NuxtLink
+                to="/design/button"
+                class="block h-full w-full outline-none"
+                >Boutons</NuxtLink
+              >
+            </div>
+            <!-- <ul class="sub-menu hidden flex-col">
                 <li class="sub-menu_items ml-3"></li>
                 <li class="sub-menu_items ml-3"></li>
               </ul> -->
-            </li>
-            <li class="sub-menu_title">Box</li>
-            <li class="sub-menu_title">Parapgraphes</li>
-            <li class="sub-menu_title">Heading</li>
-          </ul>
-        </nav>
-      </div>
-    </aside>
-
+          </li>
+          <li class="sub-menu_title">Box</li>
+          <li class="sub-menu_title">Parapgraphes</li>
+          <li class="sub-menu_title">Heading</li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
 </template>
 
 <style scoped lang="scss">
